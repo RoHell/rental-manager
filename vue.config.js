b0bd3a04e4287814b32cecd65a5b50790d6d3db9
RoHell <karol-roch@wp.pl>
2018-10-11 00:27:59 +0200

    vue-cli-service

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  pluginOptions: {
+    i18n: {
+      locale: 'pl',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: true
+    }
+  },
+
+  pwa: {
+    name: 'Equipment Manager'
+  }
+}
