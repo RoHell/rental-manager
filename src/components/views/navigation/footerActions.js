56118c83ab070576b5e4cd6822af862aca4305e6
DOCFLOW\karol.makowski <karol.makowski@docflow.pl>
2018-11-13 08:01:14 +0100

    refactor

---
+++ src/components/views/navigation/footerActions.js
@@ -0,0 +1,17 @@
+export const create = {
+  icon: "plus",
+  action: "CREATE",
+  route: "create"
+};
+
+export const back = {
+  icon: "arrow-left",
+  action: "BACK",
+  route: "items"
+};
+
+export const submit = {
+  icon: "check",
+  action: "SUBMIT",
+  route: "items"
+};
