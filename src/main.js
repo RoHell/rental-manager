258d00148699af329322c8fe56af66e2b7ae6ebc
DOCFLOW\karol.makowski <karol.makowski@docflow.pl>
2018-11-13 08:03:46 +0100

    firebase firestore

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import "./plugins/axios";
 import App from "./App.vue";
 import router from "./router";
 import store from "./vuex/index";
+import "./firebase";
 import "./registerServiceWorker";
+import VueFire from "vuefire";
+
+Vue.use(VueFire);
 
 Vue.config.productionTip = false;
 
