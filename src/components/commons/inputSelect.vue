46ccd5e50a8dd8b5cf2662f6eff65498f218979a
RoHell <karol-roch@wp.pl>
2018-11-25 21:25:11 +0100

    inputs

---
+++ src/components/commons/inputSelect.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+  .text-field
+    select(
+      id="inputSelect"
+      ref="inputSelect"
+      :class="{'filled': value}"
+      :value="value"
+      @input="updateValue"
+      :required="required"
+    )
+      option(
+        v-for="option in options"
+        :key="option.code"
+      ) {{ option.name }}
+    label(for="inputSelect") {{ label }}
+    .bar
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: ""
+    },
+    value: {
+      type: String,
+      default: ""
+    },
+    options: {
+      type: Array,
+      default: () => ({})
+    },
+    required: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {
+    updateValue() {
+      this.$emit("input", this.$refs.inputSelect.value);
+    }
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+  @import "../../sass/textField.sass"
+</style>
---
+++ src/components/commons/inputText.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+  .text-field
+    input(
+      id="inputText"
+      :class="{'filled': value}"
+      type="text"
+      ref="inputText"
+      :value="value"
+      @input="updateValue"
+      :required="required"
+    )
+    label(for="inputText") {{ label }}
+    .bar
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: ""
+    },
+    value: {
+      type: String,
+      default: ""
+    },
+    required: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {
+    updateValue() {
+      this.$emit("input", this.$refs.inputText.value);
+    }
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+  @import "../../sass/textField.sass"
+</style>
