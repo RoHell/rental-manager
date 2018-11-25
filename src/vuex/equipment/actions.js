b22e0434a51dda802c0f94a92f0e208f60622327
DOCFLOW\karol.makowski <karol.makowski@docflow.pl>
2018-11-25 21:17:48 +0100

    firebase firestore remove

--- src/vuex/equipment/actions.js
+++ src/vuex/equipment/actions.js
@@ -1,12 +1,9 @@
 import constants from "./constants";
-import { rentalsRef } from "../../firebase";
 
 export const prepareItem = async ({ commit }, item) => {
   commit(constants.PREPARE_ITEM, item);
 };
 
 export const addPreparedItem = async ({ commit, getters }) => {
-  console.log("getters.getPreparedItem", getters.getPreparedItem);
-  rentalsRef.push(getters.getPreparedItem);
   commit(constants.ADD_PREPARED_ITEM, getters.getPreparedItem);
 };
